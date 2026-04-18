c852ffd826c2ffbbd2b66d370fd237e3bd3a18b2
Ahmed Sualih <sualihahmed26@gmail.com>
2026-04-18 12:17:39 +0300

    Beautify header text styles

--- FoodOrderProject/src/Components/Header/Header.jsx
+++ FoodOrderProject/src/Components/Header/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-content">
-        <h2>Order your Favourite Food here</h2>
-        <p>Enjoy a wide variety of delicious dishes made with the freshest ingredients. Our menu has something for everyone — from tasty starters to mouthwatering main courses and sweet desserts. Each meal is carefully prepared to bring out rich flavors and give you a satisfying and enjoyable dining experience.</p>
+        <h2>Order your <span className="highlight">Favourite Food</span> here</h2>
+        <p>Enjoy a wide variety of delicious dishes made with the freshest ingredients. Our menu has something for everyone — from tasty starters to mouthwatering main courses and sweet desserts.</p>
         <button>View Menu</button>
       </div>
     </div>
